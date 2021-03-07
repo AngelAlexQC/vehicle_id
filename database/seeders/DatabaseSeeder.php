3acac7d38d25801b34a90cda545e6bd055ad8663
Ángel Quiroz <guirudj007@gmail.com>
2021-03-06 19:10:52 -0500

    Faker Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Adding an admin user
-        $user = \App\Models\User::factory()
+        $user = User::factory()
             ->count(1)
             ->create([
                 'name', 'Ángel Quiroz',
