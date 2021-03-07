7de6887375a4a9242b05f679e2020a4e5f3d7d5c
Ángel Quiroz <guirudj007@gmail.com>
2021-03-06 21:04:39 -0500

    Fixx Null Records on No records

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,25 +12,29 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                        <div class="container text-center">
-                            <h3>
-                                {{ $record->driver->name }}&nbsp;{{ $record->driver->surname }}
-                            </h3>
-                            <h5>
-                                Cédula: {{ $record->driver->dni }}
-                            </h5>
-                            <img src="https://picsum.photos/500" class="rounded" style="width: 15rem;height: 15rem;">
-                            <p>
-                            <ul style="list-style: none;padding-left: 0">
-                                <li>
-                                    <strong>Tel:</strong>&nbsp;{{ $record->driver->phone }}
-                                </li>
-                                <li>
-                                    <strong>Email:</strong>&nbsp;{{ $record->driver->email }}
-                                </li>
-                            </ul>
-                            </p>
-                        </div>
+                        @if ($record)
+                            <div class="container text-center">
+                                <h3>
+                                    {{ $record->driver->name }}&nbsp;{{ $record->driver->surname }}
+                                </h3>
+                                <h5>
+                                    Cédula: {{ $record->driver->dni }}
+                                </h5>
+                                <img src="https://picsum.photos/500" class="rounded" style="width: 15rem;height: 15rem;">
+                                <p>
+                                <ul style="list-style: none;padding-left: 0">
+                                    <li>
+                                        <strong>Tel:</strong>&nbsp;{{ $record->driver->phone }}
+                                    </li>
+                                    <li>
+                                        <strong>Email:</strong>&nbsp;{{ $record->driver->email }}
+                                    </li>
+                                </ul>
+                                </p>
+                            </div>
+                        @else
+                            Bienvenido...
+                        @endif
                     </div>
                 </div>
             </div>
